642c3031484bfdabee10e90786fef71ea4126505
tofa93 <tofig.valiyev1993@gmail.com>
2024-02-18 17:32:18 +0100

    Deployment

---
+++ backend/routes/workouts.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const Workout = require('../models/workoutModel')
+
+const router = express.Router()
+
+// GET all workouts
+router.get('/', (req, res) => {
+    res.json({ mssg: "GET all workouts" })
+})
+
+// GET a sinle workout
+router.get('/:id', (req, res) => {
+    res.json({ mssg: "GET a single workout" })
+})
+
+// POST a new workout
+router.post('/', async (req, res) => {
+    const { title, load, reps } = req.body
+    try {
+        const workout = await Workout.create({ title, load, reps })
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+    res.json({ mssg: "POST a new workout" })
+})
+
+// DELETE a new workout
+router.delete('/:id', (req, res) => {
+    res.json({ mssg: "Delete a workout" })
+})
+
+// Update a new workout
+router.patch('/:id', (req, res) => {
+    res.json({ mssg: "Update a workout" })
+})
+
+module.exports = router
